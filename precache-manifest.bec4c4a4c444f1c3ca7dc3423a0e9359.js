9f04d2c07eeaf980d9bd167755ff0998ab64b749
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2019-04-12 19:41:07 -0400

    Updates

---
+++ precache-manifest.bec4c4a4c444f1c3ca7dc3423a0e9359.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "da76ff0a6070e7656a4456009ddca009",
+    "url": "/Cats-and-Presidents-Redux/static/media/greyfloral_@2X.da76ff0a.png"
+  },
+  {
+    "revision": "52b508cda49d3bf702e6867cc71a1e2d",
+    "url": "/Cats-and-Presidents-Redux/static/media/fabric_of_squares_gray_@2X.52b508cd.png"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Cats-and-Presidents-Redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "87a58a4e882594ba43c1",
+    "url": "/Cats-and-Presidents-Redux/static/js/runtime~main.7029ce2b.js"
+  },
+  {
+    "revision": "da17de4e613c37ab37f8",
+    "url": "/Cats-and-Presidents-Redux/static/js/main.61629c68.chunk.js"
+  },
+  {
+    "revision": "6a8374dd94b302c9b7a0",
+    "url": "/Cats-and-Presidents-Redux/static/js/2.e0375a9f.chunk.js"
+  },
+  {
+    "revision": "da17de4e613c37ab37f8",
+    "url": "/Cats-and-Presidents-Redux/static/css/main.09c6ea59.chunk.css"
+  },
+  {
+    "revision": "6a8374dd94b302c9b7a0",
+    "url": "/Cats-and-Presidents-Redux/static/css/2.5ffe944c.chunk.css"
+  },
+  {
+    "revision": "4e7dd77df7d425d6e6a978d28d8d1d7f",
+    "url": "/Cats-and-Presidents-Redux/index.html"
+  }
+];
